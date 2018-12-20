900440529a04ca7a751d3e8a24fd5ba842949dcd
mfherman <mfherman@gmail.com>
2018-12-20 10:27:23 -0500

    add cd_sf docs

--- R/sf_data.R
+++ R/sf_data.R
@@ -5,9 +5,9 @@
 #'
 #' @format An sf object with 2166 rows and 13 variables:
 #' \describe{
-#'   \item{boro_tract_id}{NYC DCP borough code and census tract number}
 #'   \item{geoid}{Census Bureau GEOID; can be used to join spatial data with
 #'   Census estimates}
+#'   \item{boro_tract_id}{NYC DCP borough code and census tract number}
 #'   \item{state_fips}{ANSI state FIPS code}
 #'   \item{county_fips}{ANSI county FIPS code}
 #'   \item{tract_id}{Census Bureau tract number}
@@ -118,3 +118,56 @@
 
 #' @rdname pumas_sf
 "pumas_sf_simple"
+
+#' NYC census block boundaries
+#'
+#' A simple features (sf) dataset containing the geometry of all census blocks
+#' in New York City from the 2010 Census.
+#'
+#' @format An sf object with 38796 rows and 14 variables:
+#' \describe{
+#'   \item{geoid}{Census Bureau GEOID; can be used to join spatial data with
+#'   Census estimates}
+#'   \item{boro_tract_block_id}{NYC DCP borough code and census tract number}
+#'   \item{state_fips}{ANSI state FIPS code}
+#'   \item{county_fips}{ANSI county FIPS code}
+#'   \item{block_id}{Census Bureau block number}
+#'   \item{tract_id}{Census Bureau tract number}
+#'   \item{county_name}{County name}
+#'   \item{boro_name}{Borough name}
+#'   \item{boro_id}{NYC DCP borough code and census tract number}
+#'   \item{nta_id}{NYC neighborhood tabulation area id}
+#'   \item{nta_name}{NYC neighborhood tabulation area name}
+#'   \item{puma_id}{Census Bureau public use microdata area id}
+#'   \item{puma_name}{Census Bureau public use microdata area name}
+#'   \item{geometry}{sfc_MULTIPOLYGON \cr
+#'   NAD83 / New York Long Island (ftUS); EPSG:2263}
+#' }
+#'
+#' @source <https://www1.nyc.gov/site/planning/data-maps/open-data/districts-download-metadata.page>
+"blocks_sf"
+
+#' NYC community district boundaries
+#'
+#' A simple features (sf) dataset containing the geometry of all community
+#' districts (CDs) and joint interest areas in New York City.
+#'
+#' @format An sf object with 71 rows and 9 variables:
+#' \describe{
+#'   \item{boro_cd_id}{NYC borough and community district id; unique ids}
+#'   \item{cd_id}{NYC community district id; ids repeat across boroughs}
+#'   \item{cd_name}{NYC community district names}
+#'   \item{state_fips}{ANSI state FIPS code}
+#'   \item{county_fips}{ANSI county FIPS code}
+#'   \item{county_name}{County name}
+#'   \item{boro_name}{Borough name}
+#'   \item{boro_id}{NYC DCP borough code and census tract number}
+#'   \item{geometry}{sfc_MULTIPOLYGON \cr
+#'   NAD83 / New York Long Island (ftUS); EPSG:2263}
+#' }
+#'
+#' @source <https://www1.nyc.gov/site/planning/data-maps/open-data.page#other>
+"cds_sf"
+
+#' @rdname cds_sf
+"cds_sf_simple"
