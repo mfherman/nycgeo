f687cc02e193835e78c40587f190d3341c3711b8
mfherman <mfherman@gmail.com>
2018-12-31 15:57:29 -0500

    add ga trackiing id

--- data-raw/hex-sticker.R
+++ data-raw/hex-sticker.R
@@ -34,3 +34,26 @@ sticker(
   )
 
 
+sticker(
+  puma_plot,
+  package = "nycgeo",
+  p_family = "source-sans",
+  p_size = 8,
+  p_y = 0.5,
+  h_fill = "#ACBFBF",
+  h_color = "#F9B693",
+  s_x = 1,
+  s_y = 1.18,
+  s_width = 2,
+  s_height = 1.25,
+  filename = "man/figures/logo-hi.png",
+  url = "nycgeo.mattherman.info",
+  u_family = "incons",
+  u_color = "#454D4D",
+  u_size = 1.3,
+  u_x = 0.35,
+  u_y = 1.55,
+  dpi = 600
+)
+
+
